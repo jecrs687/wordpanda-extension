fabcaa1a8eb05de51f32219ca686c18227184f90
Emanuel Cascone <emanuelcascone@gmail.com>
2023-04-27 20:32:04 +0200

    feat: capture traffic

--- background/back.js
+++ background/back.js
@@ -2,7 +2,9 @@
 const injector = document.createElement('script')
 injector.content = "script-src 'self' 'unsafe-inline''./background.js'"
 const getValues = async () => {
-        const values = window.performance.getEntries().map((value)=>value?.name?.split('.')).filter((v)=> v.includes('ttml2')).map((v)=>v.join("."))
+        var performance = window.performance || window.mozPerformance 
+                   || window.msPerformance || window.webkitPerformance;
+        const values = performance.getEntries().map((value)=>value?.name?.split('.')).filter((v)=> v.includes('ttml2')).map((v)=>v.join("."))
         if(!values.length)
         setTimeout(()=> {getValues()}, 1000)
         console.log({values})
