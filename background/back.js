2694ac0d73bd17004b92e774ba89905c6d3b0a67
Emanuel Cascone <emanuelcascone@gmail.com>
2023-04-27 20:31:30 +0200

    feat: capture network traffic

--- background/back.js
+++ background/back.js
@@ -1,4 +1,13 @@
 
 const injector = document.createElement('script')
 injector.content = "script-src 'self' 'unsafe-inline''./background.js'"
-(document.head || document.documentElement).appendChild(injector)
+const getValues = async () => {
+        const values = window.performance.getEntries().map((value)=>value?.name?.split('.')).filter((v)=> v.includes('ttml2')).map((v)=>v.join("."))
+        if(!values.length)
+        setTimeout(()=> {getValues()}, 1000)
+        console.log({values})
+}
+window.addEventListener('load', ()=> {
+        console.log('loaded')
+        getValues()
+})
